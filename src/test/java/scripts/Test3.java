ec750ee8f4e64082bc9b98e0c388aca5317d5ae6
asus <asus@LAPTOP-T09IHNU0>
2022-12-25 17:34:28 +0530

    hi

---
+++ src/test/java/scripts/Test3.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	@Test
+	public void tc3() {
+		Reporter.log("Hello Python",true);
+	}
+
+}
