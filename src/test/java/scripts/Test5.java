f931c2b2edaf577eb9cfacf7d431c261ff42df91
asus <asus@LAPTOP-T09IHNU0>
2022-12-25 21:40:31 +0530

    hi

---
+++ src/test/java/scripts/Test5.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	
+	@Test
+	public void tc5() {
+		Reporter.log("Hello API", true);
+	}
+
+}
---
+++ src/test/java/scripts/Test6.java
@@ -0,0 +1,13 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+	
+	@Test
+	public void tc6() {
+		Reporter.log("Hello Postman", true);
+	}
+
+}
